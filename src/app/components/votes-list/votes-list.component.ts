c672a202bc02f5693cd7e0ccaa9381209cc81943
aziza_31 <aziza.kalmasova@phystech.edu>
2019-09-15 09:44:16 +0300

    votes list

--- src/app/components/votes-list/votes-list.component.ts
+++ src/app/components/votes-list/votes-list.component.ts
@@ -5,6 +5,7 @@ import {VotesService} from '../../services/votes/votes.service';
 import {ShortVoteDtoInterface} from '../../services/votes/dto/short-vote.dto';
 import {Observable} from 'rxjs/Observable';
 import {VotesServiceStub} from '../../services/votes/stub/votes.service.stub';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-votes-list',
@@ -15,10 +16,15 @@ import {VotesServiceStub} from '../../services/votes/stub/votes.service.stub';
 export class VotesListComponent implements OnInit {
   public votes: Observable<ShortVoteDtoInterface[]>;
 
-  constructor(public votesService: VotesServiceStub) { }
+  constructor(public votesService: VotesServiceStub, private router: Router) { }
   ngOnInit() {
     this.votes = this.getShortVotes$();
   }
+
+  public navigateToVote(vote_id: number): void {
+    this.router.navigate(['votes-list', `${vote_id}`]);
+  }
+
   private getShortVotes$(): Observable<ShortVoteDtoInterface[]> {
     return this.votesService.fetchVotes$();
   }
